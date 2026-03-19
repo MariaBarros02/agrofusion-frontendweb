00ab3916dae5ceed47064e7f49a73a7035f4edd6
María Alejandra Barros Murillo <141078532+MariaBarros02@users.noreply.github.com>
2026-03-18 22:07:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait-primary',
-        scope: '/agrofusion/',
-        start_url: '/agrofusion/',
+        scope: '/agrofusionTest/',
+        start_url: '/agrofusionTest/',
         
         icons: [
           {
@@ -65,7 +65,7 @@ export default defineConfig({
       },
     }),
   ],
-  base: "/agrofusion/",
+  base: "/agrofusionTest/",
   server: {
     allowedHosts: ['inmero.co', 'www.inmero.co'] 
   },
