b042e79d0a14f10286ad8e774ddecfe4c073ce62
María Alejandra Barros Murillo <141078532+MariaBarros02@users.noreply.github.com>
2026-04-04 22:59:25 -0500

    Change navigateFallback path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
 
-        navigateFallback: '/agrofusion/index.html',
+        navigateFallback: '/index.html',
         navigateFallbackDenylist: [/^\/api/],
         runtimeCaching: [
           {
