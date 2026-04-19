2c49127f013e5a128ac7eb30da837cfba937681b
MariaBarros02 <141078532+MariaBarros02@users.noreply.github.com>
2026-04-19 16:16:59 -0500

    Fix:espera de autenticación arreglada

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -87,7 +87,7 @@ export const applyAuthInterceptor = (api: AxiosInstance) => {
 
           originalRequest._retry = true;
           isRefreshing = true;
-
+          store.setRefreshing(true);
           const refreshInstance = axios.create({ baseURL: originalRequest.baseURL });
           try {
             /** * Intento de renovar el Access Token usando el Refresh Token 
@@ -105,16 +105,18 @@ export const applyAuthInterceptor = (api: AxiosInstance) => {
               `Bearer ${access_token}`;
 
             return api(originalRequest);
+
           } catch (refreshError) {
             /** * Si el refresco falla (ej: Refresh Token expirado), 
              * limpiamos todo y redirigimos al login.
              */
             store.logout();
             localStorage.removeItem("auth-storage");
-            window.location.href = "/login";
+            store.logout();            
             return Promise.reject(refreshError);
           } finally {
             isRefreshing = false;
+            store.setRefreshing(false);
           }
         }
       }
