16b73163bf7f7419e0a29be24de8f3a9bb95a0f1
MariaBarros02 <141078532+MariaBarros02@users.noreply.github.com>
2026-05-11 15:17:15 -0500

    Fix: Diseño responsive

--- src/pages/roles/CreateRole.tsx
+++ src/pages/roles/CreateRole.tsx
@@ -32,15 +32,12 @@ interface CreateValues {
   permissions: PermissionBasicResponse[];
 }
 
-const badgeColors = [
-  "info",
-  "failure",
-  "success",
-  "warning",
-  "indigo",
-  "purple",
-  "pink",
-] as const;
+const actionColors: Record<string, string> = {
+  CREATE: "success",
+  READ: "info",
+  UPDATE: "warning",
+  DELETE: "failure",
+};
 const CreateRole = () => {
   const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
@@ -123,7 +120,8 @@ const CreateRole = () => {
 
         setAlert({
           message: t(`errors.${errorCode}`),
-          type: errorCode == "AUTH_INSUFFICIENT_PERMISSIONS" ? "warning": "error",
+          type:
+            errorCode == "AUTH_INSUFFICIENT_PERMISSIONS" ? "warning" : "error",
         });
       }
     },
@@ -152,13 +150,63 @@ const CreateRole = () => {
         (assigned) => assigned.permission_id === available.permission_id,
       ),
   );
+  const groupedAvailablePermissions = filteredAvailablePermissions.reduce(
+    (acc, perm) => {
+      const module = perm.module_code || "unknown";
 
+      if (!acc[module]) {
+        acc[module] = {
+          module_name: perm.module_name || t("common.unknownModule"),
+          permissions: [],
+        };
+      }
+
+      acc[module].permissions.push(perm);
+
+      return acc;
+    },
+    {} as Record<
+      string,
+      {
+        module_name: string;
+        permissions: typeof filteredAvailablePermissions;
+      }
+    >,
+  );
+
+  const groupedRolePermissions = rolePermissions.reduce(
+    (acc, perm) => {
+      const module = perm.module_code || "unknown";
+
+      if (!acc[module]) {
+        acc[module] = {
+          module_name: perm.module_name || t("common.unknownModule"),
+          permissions: [],
+        };
+      }
+
+      acc[module].permissions.push(perm);
+
+      return acc;
+    },
+    {} as Record<
+      string,
+      {
+        module_name: string;
+        permissions: typeof rolePermissions;
+      }
+    >,
+  );
   const canAccessModule = useModuleAccessStore((s) => s.canAccessModule);
   useSubmoduleAccessStore((s) => s.loaded);
-  const canAccessSubmodule = useSubmoduleAccessStore((s) => s.canAccessSubmodule);
+  const canAccessSubmodule = useSubmoduleAccessStore(
+    (s) => s.canAccessSubmodule,
+  );
   const showModuleInactive = !canAccessModule("ADMINISTRATION");
-  const showSubmoduleInactive = canAccessModule("ADMINISTRATION") && !canAccessSubmodule("ROLES");
-  const showContent = canAccessModule("ADMINISTRATION") && canAccessSubmodule("ROLES");
+  const showSubmoduleInactive =
+    canAccessModule("ADMINISTRATION") && !canAccessSubmodule("ROLES");
+  const showContent =
+    canAccessModule("ADMINISTRATION") && canAccessSubmodule("ROLES");
 
   return (
     <AppLayoutSB>
@@ -170,170 +218,225 @@ const CreateRole = () => {
       {showSubmoduleInactive && <SubmoduleInactive />}
       {showContent && (
         <>
-      {loading && (
-        <div className="flex items-center justify-center p-3 mt-3 bg-white border shadow-sm dark:border-gray-600 dark:bg-gray-700 rounded-2xl h-1/2">
-          {" "}
-          <p className="text-3xl font-bold">{t("createRole.loading")}</p>{" "}
-        </div>
-      )}{" "}
-      {error && (
-        <div className="flex items-center justify-center mt-3 bg-white border shadow-sm dark:border-gray-600 dark:bg-gray-700 h-1/2">
-          {" "}
-          <p className="text-3xl font-bold">{t("createRole.error")}</p>{" "}
-        </div>
-      )}{" "}
-      {!loading && !error && (
-        <form
-          onSubmit={formik.handleSubmit}
-          className="p-4 m-0 bg-white border shadow-sm rounded-2xl h-[calc(100vh-130px)] overflow-auto dark:border-gray-600 dark:bg-gray-700"
-        >
-          <div className="w-full">
-            <div className="block mb-2">
-              <Label htmlFor="name">{t("createRole.name")}</Label>
+          {loading && (
+            <div className="flex items-center justify-center p-3 mt-3 bg-white border shadow-sm dark:border-gray-600 dark:bg-gray-700 rounded-2xl h-1/2">
+              {" "}
+              <p className="text-3xl font-bold">
+                {t("createRole.loading")}
+              </p>{" "}
             </div>
-            <TextInput
-              id="name"
-              type="text"
-              sizing="sm"
-              placeholder={t("createRole.namePlaceholder")}
-              required
-              {...formik.getFieldProps("name")}
-              color={
-                formik.touched.name && formik.errors.name ? "failure" : "gray"
-              }
-            />
-            {displayError("name")}
-          </div>
-
-          <div className="w-full mt-2">
-            <div className="block mb-2">
-              <Label htmlFor="description">{t("createRole.description")}</Label>
+          )}{" "}
+          {error && (
+            <div className="flex items-center justify-center mt-3 bg-white border shadow-sm dark:border-gray-600 dark:bg-gray-700 h-1/2">
+              {" "}
+              <p className="text-3xl font-bold">{t("createRole.error")}</p>{" "}
             </div>
-            <Textarea
-              id="description"
-              placeholder={t("createRole.descripPlaceholder")}
-              {...formik.getFieldProps("description")}
-              color={
-                formik.touched.description && formik.errors.description
-                  ? "failure"
-                  : "gray"
-              }
-            />
-            {displayError("description")}
-          </div>
-          <div className="flex items-end gap-5 mt-2">
-            <div className="w-full">
-              <div className="block mb-2">
-                <Label htmlFor="code">{t("createRole.code")}</Label>
-              </div>
-
-              <TextInput
-                id="code"
-                type="text"
-                sizing="sm"
-                placeholder={t("createRole.codePlaceholder")}
-                required
-                {...formik.getFieldProps("code")}
-                color={
-                  formik.touched.code && formik.errors.code ? "failure" : "gray"
-                }
-              />
-
-              {displayError("code")}
-            </div>
-
-            <div className="w-full ">
-              <Label htmlFor="state">{t("createRole.state")}</Label>
-
-              <div className="flex items-center gap-3 mt-2">
-                <ToggleSwitch
-                  checked={formik.values.state === "ACTIVE"}
-                  label={
-                    formik.values.state === "ACTIVE"
-                      ? t("common.active")
-                      : t("common.inactive")
+          )}{" "}
+          {!loading && !error && (
+            <form
+              onSubmit={formik.handleSubmit}
+              className="p-4 m-0 bg-white border shadow-sm rounded-2xl h-[calc(100vh-130px)] overflow-auto dark:border-gray-600 dark:bg-gray-700"
+            >
+              <div className="w-full">
+                <div className="block mb-2">
+                  <Label htmlFor="name">{t("createRole.name")}</Label>
+                </div>
+                <TextInput
+                  id="name"
+                  type="text"
+                  sizing="sm"
+                  placeholder={t("createRole.namePlaceholder")}
+                  required
+                  {...formik.getFieldProps("name")}
+                  color={
+                    formik.touched.name && formik.errors.name
+                      ? "failure"
+                      : "gray"
                   }
-                  disabled={loading}
-                  onChange={handleToggleState}
-                  color="red"
                 />
+                {displayError("name")}
               </div>
-            </div>
-          </div>
-          <div className="w-full mt-4">
-            <Label>{t("createRole.permissionsAssigned")}</Label>
 
-            <div className="flex flex-wrap gap-2 mt-2">
-              {rolePermissions.map((permission, index) => (
-                <Badge
-                  key={permission.permission_id}
-                  color={badgeColors[index % badgeColors.length]}
-                  className="flex items-center gap-2 cursor-pointer"
-                >
-                  {permission.permission_name}
-                  <span
-                    onClick={() =>
-                      handleRemovePermission(permission.permission_id)
+              <div className="w-full mt-2">
+                <div className="block mb-2">
+                  <Label htmlFor="description">
+                    {t("createRole.description")}
+                  </Label>
+                </div>
+                <Textarea
+                  id="description"
+                  placeholder={t("createRole.descripPlaceholder")}
+                  {...formik.getFieldProps("description")}
+                  color={
+                    formik.touched.description && formik.errors.description
+                      ? "failure"
+                      : "gray"
+                  }
+                />
+                {displayError("description")}
+              </div>
+              <div className="flex items-end gap-5 mt-2">
+                <div className="w-full">
+                  <div className="block mb-2">
+                    <Label htmlFor="code">{t("createRole.code")}</Label>
+                  </div>
+
+                  <TextInput
+                    id="code"
+                    type="text"
+                    sizing="sm"
+                    placeholder={t("createRole.codePlaceholder")}
+                    required
+                    {...formik.getFieldProps("code")}
+                    color={
+                      formik.touched.code && formik.errors.code
+                        ? "failure"
+                        : "gray"
                     }
-                    className="ml-1 font-bold cursor-pointer"
-                  >
-                    ✕
-                  </span>
-                </Badge>
-              ))}
-            </div>
-            {formik.touched.permissions && formik.errors.permissions && (
-              <p className="mt-1 text-sm text-red-500">
-                {formik.errors.permissions as string}
-              </p>
-            )}
+                  />
 
-            <Label className="block mt-4">
-              {t("createRole.permissionsAvailable")}
-            </Label>
+                  {displayError("code")}
+                </div>
 
-            <div className="flex flex-wrap gap-2 mt-2">
-              {filteredAvailablePermissions.map((permission) => (
-                <Badge
-                  key={permission.permission_id}
-                  color="gray"
-                  className="cursor-pointer"
-                  onClick={() => handleAddPermission(permission)}
+                <div className="w-full ">
+                  <Label htmlFor="state">{t("createRole.state")}</Label>
+
+                  <div className="flex items-center gap-3 mt-2">
+                    <ToggleSwitch
+                      checked={formik.values.state === "ACTIVE"}
+                      label={
+                        formik.values.state === "ACTIVE"
+                          ? t("common.active")
+                          : t("common.inactive")
+                      }
+                      disabled={loading}
+                      onChange={handleToggleState}
+                      color="red"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full mt-4">
+                <div className="flex justify-between">
+                  <Label>{t("createRole.permissionsAssigned")}</Label>
+
+                  <div className="flex flex-wrap items-center gap-2 mb-3">
+                    <span className="text-sm font-bold text-gray-600">
+                      {t("viewRole.actionLegend")}:
+                    </span>
+
+                    {Object.entries(actionColors).map(([action, color]) => (
+                      <Badge key={action} color={color}>
+                        {t(`common.${action}`)}
+                      </Badge>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="mt-3 space-y-4">
+                  {Object.entries(groupedRolePermissions || {}).map(
+                    ([moduleCode, moduleData]) => (
+                      <div key={moduleCode}>
+                        <h3 className="mb-2 text-xs font-bold text-gray-500 dark:text-gray-300">
+                          {moduleData.module_name}
+                        </h3>
+
+                        <div className="flex flex-wrap gap-2">
+                          {moduleData.permissions.map((permission) => (
+                            <Badge
+                              key={permission.permission_id}
+                              color={
+                                actionColors[permission.action_name || ""] ||
+                                "gray"
+                              }
+                              className="flex items-center gap-2 cursor-pointer"
+                            >
+                              {permission.permission_name}
+
+                              <span
+                                onClick={() =>
+                                  handleRemovePermission(
+                                    permission.permission_id,
+                                  )
+                                }
+                                className="ml-1 font-bold cursor-pointer"
+                              >
+                                ✕
+                              </span>
+                            </Badge>
+                          ))}
+                        </div>
+                      </div>
+                    ),
+                  )}
+                </div>
+                {formik.touched.permissions && formik.errors.permissions && (
+                  <p className="mt-1 text-sm text-red-500">
+                    {formik.errors.permissions as string}
+                  </p>
+                )}
+
+                <Label className="block mt-4">
+                  {t("createRole.permissionsAvailable")}
+                </Label>
+
+                <div className="mt-5 space-y-4">
+                  {Object.entries(groupedAvailablePermissions || {}).map(
+                    ([moduleCode, moduleData]) => (
+                      <div key={moduleCode}>
+                        <h3 className="mb-2 text-xs font-bold text-gray-500 dark:text-gray-300">
+                          {moduleData.module_name}
+                        </h3>
+
+                        <div className="flex flex-wrap gap-2">
+                          {moduleData.permissions.map((permission) => (
+                            <Badge
+                              key={permission.permission_id}
+                              color={
+                                actionColors[permission.action_name || ""] ||
+                                "gray"
+                              }
+                              className="cursor-pointer"
+                              onClick={() => handleAddPermission(permission)}
+                            >
+                              + {permission.permission_name}
+                            </Badge>
+                          ))}
+                        </div>
+                      </div>
+                    ),
+                  )}
+                </div>
+              </div>
+
+              <div className="flex justify-end gap-3 mt-3">
+                <Button
+                  onClick={() => navigate(`/administration/roles`)}
+                  color="alternative"
                 >
-                  + {permission.permission_name}
-                </Badge>
-              ))}
-            </div>
-          </div>
-
-          <div className="flex justify-end gap-3 mt-3">
-            <Button
-              onClick={() => navigate(`/administration/roles`)}
-              color="alternative"
-            >
-              {t("common.cancel")}
-            </Button>
-            <Button
-              type="submit"
-              color="blue"
-              disabled={!formik.isValid || formik.isSubmitting}
-            >
-              <FiSave size={22} className="mr-1" />
-              {t("createRole.save")}
-            </Button>
-          </div>
-        </form>
-      )}
-      {alert && (
-        <AlertSimple
-          message={t(alert.message)}
-          type={alert.type}
-          to={alert.to}
-          onClose={() => {
-            setAlert(null);
-          }}
-        />
-      )}
+                  {t("common.cancel")}
+                </Button>
+                <Button
+                  type="submit"
+                  color="blue"
+                  disabled={!formik.isValid || formik.isSubmitting}
+                >
+                  <FiSave size={22} className="mr-1" />
+                  {t("createRole.save")}
+                </Button>
+              </div>
+            </form>
+          )}
+          {alert && (
+            <AlertSimple
+              message={t(alert.message)}
+              type={alert.type}
+              to={alert.to}
+              onClose={() => {
+                setAlert(null);
+              }}
+            />
+          )}
         </>
       )}
     </AppLayoutSB>
